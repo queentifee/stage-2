d9b9b6cba2bb9181bbfcd5ce9e1251291a9e42d5
Queen <samuellqueenie@gmail.com>
2025-10-27 14:43:15 +0100

    Countries API

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const express = require("express");
 const countryRoutes = require("./routes/countryRoutes");
-
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-require ("dotenv").config();
+
 
 
 app.use("/", countryRoutes);
 
-app.listen(process.env.PORT, () =>
-  console.log("Server running...")
-);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
